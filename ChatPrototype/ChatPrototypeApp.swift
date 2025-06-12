24b26556381262d0d0206d89dcb3ddd4ed6d5039
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-06-12 21:36:43 +0600

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Md. Atiqul Islam on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
